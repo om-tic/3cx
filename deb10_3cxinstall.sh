ad98838c1573ff0546e51025d47f54dcd24d14c9
om-tic <109603322+om-tic@users.noreply.github.com>
2022-07-19 10:16:07 -0400

    Update deb10_3cxinstall.sh

--- deb10_3cxinstall.sh
+++ deb10_3cxinstall.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 apt-get update
+wait
 apt-get upgrade
+wait
 apt-get install gnupg2
+wait
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
+wait
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | tee /etc/apt/sources.list.d/3cxpbx.list
+wait
 apt-get update
+wait
 apt-get install net-tools dphys-swapfile
 #apt-get install 3cxpbx
