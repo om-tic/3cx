144c557be064a48e932624facd5eb1c62add2d77
om-tic <109603322+om-tic@users.noreply.github.com>
2022-07-19 09:48:41 -0400

    Update deb10_3cxinstall.sh

--- deb10_3cxinstall.sh
+++ deb10_3cxinstall.sh
@@ -5,7 +5,7 @@ apt-get upgrade -y
 echo "Mise à jour Linux complété"
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
 echo "Ajout de la clé 3CX complété"
-apt-get apt install gnupg2 -y
+apt-get install gnupg2 -y
 echo "Installation de gnupg2 complété"
 echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | tee /etc/apt/sources.list.d/3cxpbx.list
 echo "Ajout de la source 3CX"
