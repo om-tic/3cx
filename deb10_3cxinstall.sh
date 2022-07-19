63387118725e53f6330325f61f36111a3fd92cc2
om-tic <109603322+om-tic@users.noreply.github.com>
2022-07-19 09:42:32 -0400

    Update deb10_3cxinstall.sh

--- deb10_3cxinstall.sh
+++ deb10_3cxinstall.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Installation de 3CX en cours"
-sudo apt update
-sudo apt upgrade -y
+sudo apt update &
+sudo apt upgrade -y &
 echo "Mise à jour Linux complété"
-wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
+wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add - &
 echo "Ajout de la clé 3CX complété"
-sudo apt install gnupg2 -y
+sudo apt install gnupg2 -y &
 echo "Installation de gnupg2 complété"
-echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
+echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list &
 echo "Ajout de la source 3CX"
-sudo apt install net-tools dphys-swapfile -y
+sudo apt install net-tools dphys-swapfile -y &
 echo "Installation de net-tools et dphys-swapfile complété"
-sudo apt update
+sudo apt update &
 sudo apt install 3cxpbx -y
