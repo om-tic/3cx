b4757f463051e938e350701b95cebb3ce1382eb9
om-tic <109603322+om-tic@users.noreply.github.com>
2022-07-19 10:03:30 -0400

    Update deb10_3cxinstall.sh

--- deb10_3cxinstall.sh
+++ deb10_3cxinstall.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-
-
 apt-get update
 apt-get --with-new-pkgs upgrade -y
-
+apt-get -y gnupg2
 # Install 3cxpbx
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | tee /etc/apt/sources.list.d/3cxpbx.list
