723188fb6e22c3102425e6fc3b03edb7299e6f86
om-tic <109603322+om-tic@users.noreply.github.com>
2022-07-19 10:53:34 -0400

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get -y upgrade
 apt-get install -y gnupg2
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | apt-key add -
-echo "deb http://downloads-global.3cx.com/downloads/debian buster main" | tee /etc/apt/sources.list.d/3cxpbx.list
+echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | tee /etc/apt/sources.list.d/3cxpbx.list
 apt-get update
 apt-get install -y net-tools dphys-swapfile
 apt-get install -y 3cxpbx
